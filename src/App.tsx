b475d1518d54a8c72410a127d43c4503d05e68f1
Celdama <celdama.dev@gmail.com>
2022-05-23 13:46:43 +0200

    src: create TestLog component to check if register user work

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ReactElement } from 'react';
 
 import TestComponent from './components/TestComponent';
+import { TestLog } from './components/TestLog';
 
 const App = (): ReactElement => (
   <div className="App">
     <h1>Budget Manager App</h1>
     <TestComponent />
+    <br />
+    <br />
+    <br />
+    <TestLog />
   </div>
 );
 
---
+++ src/components/TestLog/index.tsx
@@ -0,0 +1,5 @@
+import React, { ReactElement } from 'react';
+
+export const TestLog = (): ReactElement => (
+  <div>TestLog</div>
+);
