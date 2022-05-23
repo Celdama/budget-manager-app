744e3b75df750d0a6ff4539748eb677ba55ce83b
Celdama <celdama.dev@gmail.com>
2022-05-23 12:43:46 +0200

    store/useStore: remove createFilterSlice

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,16 +1,12 @@
 import create from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-import createFilterSlice, { FilterSlice } from './createFilterSlice';
 import createUserSlice, { UserSlice } from './createUserSlice';
 
-// import createTodoSlice, { TodoSlice } from './createTodoSlice';
-export type MyState = FilterSlice & UserSlice
+export type MyState = UserSlice
 
 const useStore = create<MyState>()(
   devtools((set, get) => ({
-    // ...createTodoSlice(set, get),
-    ...createFilterSlice(set, get),
     ...createUserSlice(set, get),
   })),
 );
