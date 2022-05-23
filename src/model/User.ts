87a0c14695b85e3d4ad485b40827a37a00ee5907
Celdama <celdama.dev@gmail.com>
2022-05-23 11:54:26 +0200

    model/User: create

---
+++ src/model/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  uid: string,
+  avatar: string,
+  username: string,
+  email: string,
+  amount: number,
+  investAmount: number,
+  totalAmount: number,
+  transactionId: string
+}
