e381290a0aec5572ecf17322bacd4bdbf1cdac0c
Celdama <celdama.dev@gmail.com>
2022-05-24 08:09:19 +0200

    App: add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
-
-import { TestLog } from './components/TestLog';
-import TestSignIn from './components/TestSignIn';
-import { Home } from './pages/Home';
+import { Router } from './router';
 
 const App = (): JSX.Element => (
   <div className="App">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<TestLog />} />
-      <Route path="/signin" element={<TestSignIn />} />
-    </Routes>
-    {/* <TestComponent />
-    <br />
-    <br />
-    <br />
-    <TestLog />
-    <br />
-    <br />
-    <br />
-    <br />
-    <TestSignIn /> */}
+    <Router />
   </div>
 );
 
