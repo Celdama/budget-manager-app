be1c6afb8a82cf996e0c5c2315ced4229e5510e5
Celdama <celdama.dev@gmail.com>
2022-05-24 08:14:41 +0200

    router/index: add new route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
+import TestComponent from '../components/TestComponent';
 import { TestLog } from '../components/TestLog';
 import TestSignIn from '../components/TestSignIn';
 import { Home } from '../pages/Home';
@@ -7,6 +8,7 @@ import { Home } from '../pages/Home';
 export const Router = (): JSX.Element => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/transaction" element={<TestComponent />} />
     <Route path="/register" element={<TestLog />} />
     <Route path="/signin" element={<TestSignIn />} />
   </Routes>
