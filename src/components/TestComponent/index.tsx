7b44c1052e86d24be04e60bf5c75b0379c4fe6f1
Celdama <celdama.dev@gmail.com>
2022-05-24 09:22:49 +0200

    components/TestComponent/index: add form to track income or expense

--- src/components/TestComponent/index.tsx
+++ src/components/TestComponent/index.tsx
@@ -2,32 +2,54 @@
 /* eslint-disable object-curly-newline */
 // eslint-disable-next-line object-curly-newline
 import { nanoid } from 'nanoid';
-import { MouseEvent, ReactElement, useEffect, useState } from 'react';
+import { MouseEvent, useState } from 'react';
 
+import { Transaction } from '../../model/Transaction';
 import useStore from '../../store/useStore';
 import { Wrapper } from './testComponent.tw';
 
-const TestComponent = (): ReactElement => {
-  const [transactionTitle, setTransactionTitle] = useState('');
-  const { addTransaction } = useStore();
+const TestComponent = (): JSX.Element => {
+  const addTransaction = useStore((state) => state.addTransaction);
+  const transactions = useStore((state) => state.transactions);
+  const [formTransaction, setFormTransaction] = useState({
+    name: '',
+    amount: 0,
+    category: '',
+  });
+
+  const handleChange = (
+    e: React.FormEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>,
+  ): void => {
+    const { name, value } = e.target as HTMLInputElement;
+    setFormTransaction((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleAddTransaction = (e: MouseEvent<HTMLButtonElement>): void => {
     e.preventDefault();
-    if (transactionTitle.length) {
+    if (formTransaction.name.length) {
       const newTransaction = {
-        amount: 400,
-        category: 'expenses',
-        date: new Date().toString(),
-        name: transactionTitle,
+        ...formTransaction,
         uid: nanoid(),
         userId: 'OEcO44NJyZPqSaH52IpL',
+        date: new Date().toString(),
       };
       // LE TRUC C4EST QUE MAINTENANT, JE VAIS DEVOIR TROUVER L'ID DE L'USER EN QUESTION
       // POUR LUI AJOUTER L'ID DE CETTE TRANSACTION DANS SON TABLEAU DE TRANSACTIONS
       addTransaction(newTransaction);
-      setTransactionTitle('');
+      setFormTransaction({
+        name: '',
+        amount: 0,
+        category: '',
+      });
     }
   };
+
+  const transactionsList = transactions.map((transaction: Transaction) => (
+    <p key={transaction.uid}>{transaction.name}</p>
+  ));
 
   return (
     <Wrapper>
@@ -35,14 +57,33 @@ const TestComponent = (): ReactElement => {
       <form>
         <input
           type="text"
-          name="todoTitle"
-          value={transactionTitle}
-          onChange={(e) => setTransactionTitle(e.target.value)}
+          name="name"
+          value={formTransaction.name}
+          onChange={handleChange}
         />
+        <input
+          type="number"
+          name="amount"
+          value={formTransaction.amount}
+          onChange={handleChange}
+        />
+
+        <select
+          name="category"
+          value={formTransaction.category}
+          onChange={handleChange}
+        >
+          <option value="">--Please choose a category--</option>
+          <option value="expense">Expense</option>
+          <option value="income">Income</option>
+        </select>
+
         <button type="submit" onClick={(e) => handleAddTransaction(e)}>
           add
         </button>
       </form>
+      <h2>transactions List</h2>
+      {transactionsList}
     </Wrapper>
   );
 };
