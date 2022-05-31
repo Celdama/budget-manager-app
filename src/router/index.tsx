2271cce66f56bb5ad34df426443eedf1ee8f3c46
Celdama <celdama.dev@gmail.com>
2022-05-31 06:45:38 +0200

    router/index: remove comment

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,13 +4,10 @@ import { RegisterGoogle } from '../components/RegisterGoogle';
 import Transactions from '../components/Transactions';
 import { Home } from '../pages/Home';
 
-// import { RegisterGoogle } from '../components/RegisterGoogle';
 export const Router = (): JSX.Element => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/transactions" element={<Transactions />} />
-    {/* <Route path="/register" element={<Register />} /> */}
-    {/* <Route path="/signin" element={<Signin />} /> */}
     <Route path="/google" element={<RegisterGoogle />} />
   </Routes>
 );
