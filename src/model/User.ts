bb13e11647bf112abb137a42c8130c9a8396ce21
Celdama <celdama.dev@gmail.com>
2022-05-31 09:28:28 +0200

    model/User: remove useless comment

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,10 +1,6 @@
 import { AuthUser } from './AuthUser';
 
 export interface User extends AuthUser {
-  // uid: string,
-  // photoURL: string | null,
-  // displayName: string | null,
-  // email: string | null,
   amount: number,
   investAmount: number,
   totalAmount: number,
