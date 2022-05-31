ae9b106ece4d7aabb36dec52d5b889394eaa9b6d
Celdama <celdama.dev@gmail.com>
2022-05-31 10:00:11 +0200

    components/Transactions/Types/transactionsProps: currentUser

--- src/components/Transactions/Types/transactionsProps.tsx
+++ src/components/Transactions/Types/transactionsProps.tsx
@@ -1,9 +1,11 @@
 import { AuthUser } from '../../../model/AuthUser';
 import { Transaction } from '../../../model/Transaction';
+import { User } from '../../../model/User';
 
 export type TransactionsProps = {
   transactions: Transaction[];
   authUser: AuthUser;
+  currentUser: User;
   addTransaction: (transaction: Transaction) => void;
   getTransactions: (authUserId: string) => void;
   deleteTransaction: (transaction: Transaction) => void;
