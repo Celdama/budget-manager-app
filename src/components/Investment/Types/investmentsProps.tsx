b29012205f9fc6f5ce811d4e0d092f03e352bd8b
Celdama <celdama.dev@gmail.com>
2022-06-02 19:40:11 +0200

    components/Investment/Types/investmentsProps: add currentUser type

--- src/components/Investment/Types/investmentsProps.tsx
+++ src/components/Investment/Types/investmentsProps.tsx
@@ -1,10 +1,12 @@
 import { AuthUser } from '../../../model/AuthUser';
 import { Investment } from '../../../model/Investment';
+import { User } from '../../../model/User';
 
 export type InvestmentsProps = {
+  authUser: AuthUser;
+  currentUser: User;
   addInvestment: (
     investment: Investment,
     currentUserInvestAmount: number
   ) => void;
-  authUser: AuthUser
 };
