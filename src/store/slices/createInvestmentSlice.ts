1db8f3888bca27464659a2a5d4780bf27cd12b68
Celdama <celdama.dev@gmail.com>
2022-06-02 19:21:20 +0200

    store/slices/createInvestmentSlice: addInvestment

--- src/store/slices/createInvestmentSlice.ts
+++ src/store/slices/createInvestmentSlice.ts
@@ -1,3 +1,7 @@
+import { arrayUnion, doc, setDoc, updateDoc } from 'firebase/firestore';
+
+import { db } from '../../config/firebaseConfig';
+import { Investment } from '../../model/Investment';
 import { NamedSetState } from '../middlewares/middleware';
 import { MyState } from '../useStore';
 
@@ -10,9 +14,36 @@ const createInvestmentSlice = (
   get: NamedSetState<MyState>,
 ) => ({
   investments: [],
-  // addInvestment: () => {
-
-  // }
+  addInvestment: (investment: Investment, currentUserInvestAmount: number) => {
+    const { uid, userId, amount, category } = investment;
+    const investementsDoc = doc(db, 'users', userId);
+    setDoc(doc(db, 'investments', uid), {
+      ...investment,
+    })
+      .then(() => {
+        updateDoc(investementsDoc, {
+          investmentsId: arrayUnion(uid),
+          investAmount: currentUserInvestAmount + amount,
+        });
+      })
+      .then(() => {
+        set(
+          (state) => ({
+            ...state,
+            currentUser: {
+              ...state.currentUser,
+              investAmount: currentUserInvestAmount + amount,
+            },
+            investments: [...state.investments, investment],
+          }),
+          false,
+          'investmentSlice.addInvestment',
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 });
 
 export default createInvestmentSlice;
