49a6d087a6ae8fec38f2ceccf08ffd98c99efb27
Celdama <celdama.dev@gmail.com>
2022-06-02 19:34:36 +0200

    components/Investment/index: create handleChange & handleAddInvestment

--- src/components/Investment/index.tsx
+++ src/components/Investment/index.tsx
@@ -1,8 +1,42 @@
-const Investment = (): JSX.Element => (
-  <div>Investment</div>
-);
+import { nanoid } from 'nanoid';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+import useStore from '../../store/useStore';
+
+const Investment = ({ addInvestment, authUser }): JSX.Element => {
+  const [formInvestment, setFormInvestment] = useState({
+    name: '',
+    amount: 0,
+    category: '',
+  });
+
+  const handleChange = (
+    e: FormEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>,
+  ): void => {
+    const { name, value } = e.target as HTMLInputElement;
+    setFormInvestment((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handlaAddInvestment = (e: MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    if (formInvestment.name.length) {
+      const newInvestment = {
+        ...formInvestment,
+        uid: nanoid(),
+        userId: authUser.uid,
+        date: new Date().toString(),
+      };
+    }
+  };
+
+  return (<p>invest</p>);
+};
 
 export const InvestmentStore = (): JSX.Element => {
-  console.log('test');
-  return (<Investment />);
+  const addInvestment = useStore((state) => state.addInvestment);
+  const authUser = useStore((state) => state.authUser);
+  return (<Investment addInvestment={addInvestment} authUser={authUser} />);
 };
