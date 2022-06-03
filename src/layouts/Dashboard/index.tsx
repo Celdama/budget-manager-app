da883845f3ee5e6bcdf8944991d794006776d3c0
Celdama <celdama.dev@gmail.com>
2022-06-03 13:53:42 +0200

    src: implement grid dashboard

--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
-
 export const Dashboard = (): JSX.Element => (
-  <div>Dashboard</div>
+  <div className="parent">
+    <div className="div1 text-white p-4">
+      DASHBOARD + NAVBAR
+    </div>
+    <div className="div2 text-white p-4">
+      LOGIN / LOGOUT
+    </div>
+    <div className="div3 text-white p-4">
+      RESUME
+    </div>
+    <div className="div4 text-white p-4">
+      BIG CHART
+    </div>
+    <div className="div5 text-white p-4">
+      MY CARD
+    </div>
+    <div className="div6 text-white p-4">DEPOSIT</div>
+    <div className="div7 text-white p-4">INVESTMENT</div>
+    <div className="div8 text-white p-4">TRANSACTION</div>
+  </div>
 );
