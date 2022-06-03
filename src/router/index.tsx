19f19da79a65280e95b2e36216ac727fd414f905
Celdama <celdama.dev@gmail.com>
2022-06-03 13:29:22 +0200

    router/index: use Dashboard

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 
 import { RegisterGoogleStore } from '../components/RegisterGoogle';
 import { TransactionsStore } from '../components/Transactions';
-import { Home } from '../pages/Home';
+import { Dashboard } from '../layouts/Dashboard';
 
 export const Router = (): JSX.Element => (
   <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Dashboard />} />
     <Route path="/transactions" element={<TransactionsStore />} />
     <Route path="/google" element={<RegisterGoogleStore />} />
   </Routes>
