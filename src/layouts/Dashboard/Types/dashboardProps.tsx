bca3912aa92be667fd4056157a38e0de421edae9
Celdama <celdama.dev@gmail.com>
2022-06-07 07:37:37 +0200

    update dashboardProps.tsx

--- src/layouts/Dashboard/Types/dashboardProps.tsx
+++ src/layouts/Dashboard/Types/dashboardProps.tsx
@@ -1,8 +1,10 @@
 import { Transaction } from '../../../model/Transaction';
 import { AuthUser } from '../../../model/AuthUser';
+import { User } from '../../../model/User';
 
 export type DashboardProps = {
-  authUser: AuthUser
+  authUser: AuthUser;
+  currentUser: User;
   transactions: Transaction[];
   getTransactions: (authUserId: string) => void;
 }
--- src/layouts/Dashboard/index.tsx
+++ src/layouts/Dashboard/index.tsx
@@ -1,36 +1,41 @@
 import { TransactionsListStore } from '../../components/TransactionsList';
 import useStore from '../../store/useStore';
+import { DashboardProps } from './Types/dashboardProps';
 
-export const Dashboard = (): JSX.Element => (
-  <div className="parent">
-    <div className="div1 text-white p-4">
-      DASHBOARD + NAVBAR
+export const Dashboard = ({ authUser, getTransactions, transactions }: DashboardProps): JSX.Element => {
+  console.log('test');
+  return (
+    <div className="parent">
+      <div className="div1 text-white p-4">
+        DASHBOARD + NAVBAR
+      </div>
+      <div className="div2 text-white p-4">
+        LOGIN / LOGOUT
+      </div>
+      <div className="div3 text-white p-4">
+        RESUME
+      </div>
+      <div className="div4 text-white p-4">
+        BIG CHART
+      </div>
+      <div className="div5 text-white p-4">
+        MY CARD
+      </div>
+      <div className="div6 text-white p-4">DEPOSIT</div>
+      <div className="div7 text-white p-4">INVESTMENT</div>
+      <div className="div8 text-white p-4"><TransactionsListStore /></div>
     </div>
-    <div className="div2 text-white p-4">
-      LOGIN / LOGOUT
-    </div>
-    <div className="div3 text-white p-4">
-      RESUME
-    </div>
-    <div className="div4 text-white p-4">
-      BIG CHART
-    </div>
-    <div className="div5 text-white p-4">
-      MY CARD
-    </div>
-    <div className="div6 text-white p-4">DEPOSIT</div>
-    <div className="div7 text-white p-4">INVESTMENT</div>
-    <div className="div8 text-white p-4"><TransactionsListStore /></div>
-  </div>
-);
+  );
+};
 
 export const DashboardStore = (): JSX.Element => {
-  console.log('dashboard');
+  const authUser = useStore((state) => state.authUser);
   const getTransactions = useStore((state) => state.getTransactions);
   const transactions = useStore((state) => state.transactions);
 
   return (
     <Dashboard
+      authUser={authUser}
       getTransactions={getTransactions}
       transactions={transactions}
     />
