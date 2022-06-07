8bc99991b049bdff5414b57960ee390e544bd6c0
Celdama <celdama.dev@gmail.com>
2022-06-07 07:30:18 +0200

    use DashboardStore in router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 
 import { RegisterGoogleStore } from '../components/RegisterGoogle';
 import { TransactionsStore } from '../components/Transactions';
-import { Dashboard } from '../layouts/Dashboard';
+import { DashboardStore } from '../layouts/Dashboard';
 
 export const Router = (): JSX.Element => (
   <Routes>
-    <Route path="/" element={<Dashboard />} />
+    <Route path="/" element={<DashboardStore />} />
     <Route path="/transactions" element={<TransactionsStore />} />
     <Route path="/google" element={<RegisterGoogleStore />} />
   </Routes>
