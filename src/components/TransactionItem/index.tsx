efbfc8265ba53ac9312d729748b5c3ea2d5a21b9
Celdama <celdama.dev@gmail.com>
2022-06-07 07:56:44 +0200

    add TransactionItemProps into TransactionItem components

--- src/components/TransactionItem/index.tsx
+++ src/components/TransactionItem/index.tsx
@@ -1,5 +1,7 @@
-export const TransactionItem = (): JSX.Element => {
-  console.log('transaction item');
+import { TransactionItemProps } from './Types/transactionItemProps';
+
+export const TransactionItem = ({ transaction }: TransactionItemProps): JSX.Element => {
+  console.log(transaction);
   return (
     <div>transaction item</div>
   );
